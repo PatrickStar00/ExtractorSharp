2c8fefdc35f96b382541c37723b8f5b0b8dd1c3c
unknown <zhupeiji1993@126.com>
2023-06-07 19:09:30 +0800

    保存中心点到x.txt

--- ExtractorSharp/Command/ImageCommand/SaveImage.cs
+++ ExtractorSharp/Command/ImageCommand/SaveImage.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
+using System.IO.Pipes;
+using System.Text;
+using System.Windows.Shapes;
+using System.Xml.Linq;
 using ExtractorSharp.Core.Command;
 using ExtractorSharp.Core.Composition;
 using ExtractorSharp.Core.Model;
@@ -82,11 +88,36 @@ namespace ExtractorSharp.Command.ImageCommand {
                     }
                 } 
                 var max = Math.Min(indexes.Length, album.List.Count);
+
+                StringBuilder sb = new StringBuilder(max);
+                int maxW = 0;
+                int maxH = 0;
+                int maxPX = 0;
+                int maxPY = 0;
+
                 for (var i = 0; i < max; i++) {
                     if (indexes[i] < 0) {
                         continue;
                     }
                     var entity = album.List[indexes[i]];
+                    sb.Append($"{entity.Location.X} {entity.Location.Y}\r\n");
+                    if (entity.Width > maxW)
+                    {
+                        maxW = entity.Width;
+                    }
+                    if (entity.Height > maxH)
+                    {
+                        maxH = entity.Height;
+                    }
+                    if (entity.Location.X > maxPX)
+                    {
+                        maxPX = entity.Location.X;
+                    }
+                    if (entity.Location.Y > maxPY)
+                    {
+                        maxPY = entity.Location.Y;
+                    }
+
                     var name = (Increment == -1 ? indexes[i] : Increment + i).ToString();
                     while (name.Length < Digit) {
                         name = string.Concat("0", name);
@@ -102,6 +133,49 @@ namespace ExtractorSharp.Command.ImageCommand {
                     var parent = System.IO.Path.GetDirectoryName(path);
                     image.Save(path); //保存贴图
                 }
+                //保存中心点
+                var txtPath = $"{dir}{prefix}x.txt";
+                try {
+                    string dr = System.IO.Path.GetDirectoryName(txtPath);
+                    if (!Directory.Exists(dr)) {
+                        Directory.CreateDirectory(dr);
+                    }
+                    if (!File.Exists(txtPath)) {
+                        FileStream fs = File.Create(txtPath);
+                        StreamWriter sw = new StreamWriter(fs, Encoding.Default);
+                        sw.Write(sb.ToString());
+                        sw.Flush();
+                        sw.Close();
+                        fs.Close();
+                    }
+                } catch (Exception e) {
+     
+                }
+
+                var whPath = $"{dir}{prefix}wh.txt";
+                try
+                {
+                    string dr = System.IO.Path.GetDirectoryName(whPath);
+                    if (!Directory.Exists(dr))
+                    {
+                        Directory.CreateDirectory(dr);
+                    }
+                    if (!File.Exists(whPath))
+                    {
+                        FileStream fs = File.Create(whPath);
+                        StreamWriter sw = new StreamWriter(fs, Encoding.Default);
+                        sw.WriteLine(maxW + " " + maxH);
+                        sw.WriteLine(maxPX + " " + maxPY);
+                        sw.Flush();
+                        sw.Close();
+                        fs.Close();
+                    }
+                }
+                catch (Exception e)
+                {
+
+                }
+
             }
         }
 
